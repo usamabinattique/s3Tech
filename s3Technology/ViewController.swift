9e6897da70c4064c5949062350ad4448ba8dd38c
Usama bin Attique <usamabinattique@gmail.com>
2017-01-11 12:41:57 +0500

    saturday commit

--- s3Technology/ViewController.swift
+++ s3Technology/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print ("y")
+        
+        
+        print (" this is january")
     }
 
     override func didReceiveMemoryWarning() {
