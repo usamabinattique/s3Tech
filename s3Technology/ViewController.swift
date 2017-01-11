015a2209da246682912c43dd4b5b66edd8652970
Usama bin Attique <usamabinattique@gmail.com>
2017-01-11 12:40:42 +0500

    second change

--- s3Technology/ViewController.swift
+++ s3Technology/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print ("y")
+        print ("today is tuesday ")
+        print ("tomorrow will be wednesday")
     }
 
     override func didReceiveMemoryWarning() {
